ed1b78030f4b00d8a350d50f5784f518577c7370
MalteEngelund <malte.engelund@hotmail.com>
2026-01-09 12:08:09 +0100

    styling
    not complete

--- root/src/App.tsx
+++ root/src/App.tsx
@@ -31,16 +31,15 @@ function App() {
       <MainHeader 
         src="https://images.unsplash.com/photo-1497366811353-6870744d04b2?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="office"
-        bgColor="#f2f2f2"
         titleH1="Leo-Lov"
         titleH3="Lov og ret kan være et plaster! Hvis du ellers har knaster!"
       />
 
 
-      <Wrapper>    
+      <Wrapper id="retCards">    
       {advoArray.map((item) => {
     return (
-      <Card key={item.id}>
+      <Card key={item.id} >
         <CardBody>
           <h3>{item.title}</h3>
         </CardBody>
@@ -60,7 +59,6 @@ function App() {
         figTitle="INTET PROBLEM"
         text="Leo-lov har altid været det bedste advokatfirma i verden. Der er simpelthen ingen der kan måle sig med os. Vi kan løse alle problemer så længe du har penge. Hvis du mod alt forventning skulle oplevel at støde på et retsligt problem som leo-lov ikke kan løse får du halvdelen af beløbet igen."
         text2="Det er sådan vi har skabt en forretning der ikke kan andet end at vokse!"
-        bgColor="#f2f2f2"
       />
         
       <FigureSection
@@ -69,11 +67,10 @@ function App() {
         figTitle="EVIG GARANTI"
         text="Leo-lov har altid været det bedste advokatfirma i verden. Der er simpelthen ingen der kan måle sig med os. Vi kan løse alle problemer så længe du har penge. Hvis du mod alt forventning skulle oplevel at støde på et retsligt problem som leo-lov ikke kan løse får du halvdelen af beløbet igen."
         text2="Det er sådan vi har skabt en forretning der ikke kan andet end at vokse!"
-        bgColor="#f2f2f2"
       />
 
 
-      <Wrapper>    
+      <Wrapper id="Advokaterne">    
       {teamArray.map((item) => {
         return (
           <Card key={item.id}>
@@ -90,13 +87,12 @@ function App() {
       </Wrapper>
 
       <FigureSection
-        src="././assets/seymap.png"
+        src="seymap.png"
         alt="A mountain"
         figTitle="HER BOR VI"
         text="Vi har valgt at bosætte os i solen! 
         Her er dejlig varmt og ikke ret mange fattige mennesker. Det nyder vi rigtig meget. 
         “Hvis du har råd til at tage til Sechellerne har du råd til os”. Det er et af vores utallige motto´er."
-        bgColor="#f2f2f2"
       />
 
         </MaxWidth>
--- root/src/components/Wrapper/Wrapper.tsx
+++ root/src/components/Wrapper/Wrapper.tsx
@@ -3,8 +3,9 @@ import style from './wrapper.module.scss'
 
 interface WrapperInterface {
   children: React.ReactNode
+  id?: string
 }
 
-export function Wrapper({ children }: WrapperInterface) {
-  return <div className={style.wrapperClass}>{children}</div>
+export function Wrapper({ children, id }: WrapperInterface) {
+  return <div className={style.wrapperClass} id={id}>{children}</div>
 }
